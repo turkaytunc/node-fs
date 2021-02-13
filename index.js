981a660028677d574519f708cb112fb2706efec7
turkaytunc <turkaytunc00@gmail.com>
2021-02-13 16:35:06 +0300

    add: yargs

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const fs = require('fs');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
-const text = 'This is a text.';
-fs.appendFileSync('./note.txt', text);
+// const text = 'This is a text.';
+// fs.appendFileSync('./note.txt', text);
+
+// console.log(process.argv[2]);
+
+yargs.command({
+  command: 'add',
+  describe: 'Add new item',
+  handler: function () {
+    console.log('New Item Added!');
+  },
+});
+
+console.log(yargs.argv);
