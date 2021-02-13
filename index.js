27223134d53f27b63660a9b78126431f308e18a0
turkaytunc <turkaytunc00@gmail.com>
2021-02-13 19:08:28 +0300

    add: readfile

--- index.js
+++ index.js
@@ -7,25 +7,35 @@ const yargs = require('yargs');
 
 // console.log(process.argv[2]);
 
-yargs.command({
-  command: 'add',
-  describe: 'Add new item',
-  builder: {
-    title: {
-      describe: 'item title',
-      demandOption: true,
-      type: 'string',
-    },
-    body: {
-      describe: 'item body',
-      demandOption: true,
-      type: 'string',
-    },
-  },
-  handler: function (argv) {
-    console.log('Title: ' + argv.title);
-    console.log('Body: ' + argv.body);
-  },
-});
+// yargs.command({
+//   command: 'add',
+//   describe: 'Add new item',
+//   builder: {
+//     title: {
+//       describe: 'item title',
+//       demandOption: true,
+//       type: 'string',
+//     },
+//     body: {
+//       describe: 'item body',
+//       demandOption: true,
+//       type: 'string',
+//     },
+//   },
+//   handler: function (argv) {
+//     console.log('Title: ' + argv.title);
+//     console.log('Body: ' + argv.body);
+//   },
+// });
 
-yargs.parse();
+// yargs.parse();
+
+const items = fs.readFileSync('./items.json');
+const parsedData = JSON.parse(items);
+
+console.log(parsedData);
+
+parsedData.title = 'this is new title';
+parsedData.body = 'this is new body';
+
+fs.writeFileSync('./items.json', JSON.stringify(parsedData));
